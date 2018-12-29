a54d4b1e80f45dd795c369196404dfdf090b06fc
enorkus <eitvys@gmail.com>
2018-12-29 19:41:12 +0200

    Backend url configuration

--- server.js
+++ server.js
@@ -12,5 +12,9 @@ app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+'/dist/brewmon-cli/index.html'));
 });
 
+app.get('/data', (req, res) => {
+    res.json({url: process.env.BREWMON_SRV_APP_URL})
+  });
+
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
